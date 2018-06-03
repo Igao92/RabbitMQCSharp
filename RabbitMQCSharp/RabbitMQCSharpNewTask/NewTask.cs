1fbcaec36821dada49797fb7023144275d1a9c6c
Igor Gomes de Lima <igorgomeslima@hotmail.com.br>
2018-06-02 23:34:45 -0300

    Work Queues

---
+++ RabbitMQCSharp/RabbitMQCSharpNewTask/NewTask.cs
@@ -0,0 +1,51 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQCSharpNewTask
+{
+    class NewTask
+    {
+        static void Main(string[] args)
+        {
+            //Here we connect to a BROKER on the local machine - hence the localhost.
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    channel.BasicPublish(exchange: string.Empty,
+                                         routingKey: "task_queue",
+                                         basicProperties: properties,
+                                         body: body);
+
+                    Console.WriteLine($"[X] Sent Message: [{message}]");
+                }
+
+                Console.WriteLine(" Press [ENTER] to exit.");
+                Console.ReadLine();
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ RabbitMQCSharp/RabbitMQCSharpWorker/Worker.cs
@@ -0,0 +1,54 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RabbitMQCSharpWorker
+{
+    class Worker
+    {
+        public static void Main()
+        {
+            //Here we connect to a BROKER on the local machine - hence the localhost.
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                                         durable: true,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($"[X] Received {message}");
+
+                        var dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine($" [X] Done");
+
+                        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                    };
+
+                    channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+
+                    Console.WriteLine(" Waiting for messages. To exit press CTRL+C.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
